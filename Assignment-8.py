7ea93e023de79d785f33eab28d14200b134f836f
Ashfaq9700 <113584553+Ashfaq9700@users.noreply.github.com>
2022-09-26 19:58:36 +0530

    Add files via upload

---
+++ Assignment-8.py
@@ -0,0 +1,87 @@
+# 1. Write a python script to print MySirG 5 times on the screen
+"""
+sir = "MySirG"
+count = 1
+
+while(count<=5):
+    print(sir)
+    count+=1
+"""
+
+#2. Write a python script to print first 10 natural numbers
+"""
+num = 1
+while(num<=10):
+    print(num)
+    num+=1
+"""
+# 3. Write a python script to print first 10 natural numbers in reverse order
+"""
+num = 10
+while(num>=1):
+    print(num)
+    num-=1
+"""
+
+# 4. Write a python script to print first 10 odd natural numbers
+"""
+num = 1
+
+while((num%2!=0)and(num<=20)):
+    print(num)
+    num+=2
+
+"""
+
+#5. Write a python script to print first 10 odd natural numbers in reverse order
+"""
+num = 20
+
+while((num >=1)):
+    if(num%2!=0):
+        print(num)
+    num-=1;
+"""
+
+# 6. Write a python script to print first 10 even natural numbers
+"""
+num = 2;
+
+while((num <=20) and (num%2==0)):
+    print(num)
+    num+=2
+"""
+
+#7. Write a python script to print first 10 even natural numbers in reverse order
+"""
+num = 20
+while(num >=2)and(num%2==0):
+    print(num)
+    num-=2
+"""
+
+#8. Write a python script to print squares of first 10 natural numbers
+"""
+num = 1
+while(num<=10):
+    print(num**2)
+    num+=1
+"""
+
+#9. Write a python script to print cubes of first 10 natural numbers
+"""
+num = 1
+while(num<=10):
+    print(num**3)
+    num+=1
+"""
+
+# 10. Write a python script to print first 10 multiples of 5
+"""
+multiple_Num = 5
+count = 1
+
+while(count <= 10):
+    print("5*{}={}".format(count,multiple_Num*count))
+    count+=1
+"""
